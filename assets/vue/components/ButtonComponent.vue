60eaf95c43076c2542c497e993d6a486bed37668
Kheuval <hadrien.hartstein@gmail.com>
2023-09-09 12:08:41 +0200

    Init

---
+++ assets/vue/components/ButtonComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <button :class="classNames" @click="log">{{ text }}</button>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  text: string,
+  classNames: string | string[],
+}>();
+
+const log = () => {
+  console.log("It works !");
+}
+</script>
